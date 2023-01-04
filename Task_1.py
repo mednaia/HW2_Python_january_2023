6001f9d7b9106af30be87ce0a7ea0cd52ee857c0
Ekaterina <tiurina.kate@gmail.com>
2023-01-04 14:06:57 +0300

    Task_1_sum_of_digits_Var_2

--- Task_1.py
+++ Task_1.py
@@ -5,15 +5,21 @@
 
 #  Variant 1
 
-def sum_of_digit(figure):
-    sum = 0
-    for i in str(figure):
-        if i != ".":
-            sum += int(i)
-    return sum
+# def sum_of_digit(figure):
+#     sum = 0
+#     for i in str(figure):
+#         if i != ".":
+#             sum += int(i)
+#     return sum
 
 
-number = float(input('Input a real number: '))
+# number = float(input('Input a real number: '))
 
-print(f'Sum of digits {number} is {sum_of_digit(number)}')
+# print(f'Sum of digits {number} is {sum_of_digit(number)}')
 
+# Variant 2
+
+number = str(input('Input a real number: '))
+
+number = number.replace('.', '')
+print(sum(map(int,str(number))))
